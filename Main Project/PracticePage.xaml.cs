83cfcc731ccadf3ad6b3ff7d9259e7c5fdb4a6d2
Иван Сабельников <ivansabelnikov3@gmail.com>
2023-01-29 17:30:06 +0300

    Added tasks count Started refactoring2

--- Main Project/PracticePage.xaml.cs
+++ Main Project/PracticePage.xaml.cs
@@ -20,6 +20,9 @@ namespace Main_Project
     /// </summary>
     public partial class PracticePage : Page
     {
+        private byte taskNumber = 0, completedTaskNumber = 0;
+        private short result;
+
         public PracticePage()
         {
             InitializeComponent();
@@ -33,82 +36,80 @@ namespace Main_Project
         private void TextBoxExResult_TextChanged(object sender, TextChangedEventArgs e)
         {
 
-        }
-
-        short result;
+        }       
 
         public string CreateExercise()
         {
-            Random rand = new Random();
-
+            Random rand = new Random();      
+            
             bool predifficult = rand.Next(100) < 50 ? true : false;
             if (predifficult == true)
-            {
-                short sl_first, sl_second;
-                do
                 {
-                    sl_first = (short)rand.Next(-30, 30);
-                    sl_second = (short)rand.Next(-30, 30);
-
-                    if (sl_first > sl_second && sl_first >= 0)
+                    short sl_first, sl_second;
+                    do
                     {
-                        if (sl_second > 0)
-                            sl_first = (short)rand.Next(0, sl_second);
-                        else
-                            sl_first = (short)rand.Next(0, sl_second * -1);
+                        sl_first = (short)rand.Next(-30, 30);
+                        sl_second = (short)rand.Next(-30, 30);
+
+                        if (sl_first > sl_second && sl_first >= 0)
+                        {
+                            if (sl_second > 0)
+                                sl_first = (short)rand.Next(0, sl_second);
+                            else
+                                sl_first = (short)rand.Next(0, sl_second * -1);
+                        }
+                        else if (sl_second > sl_first && sl_second >= 0)
+                        {
+                            sl_first = (short)rand.Next(sl_second, 30);
+
+                        }
                     }
-                    else if (sl_second > sl_first && sl_second >= 0)
-                    {
-                        sl_first = (short)rand.Next(sl_second, 30);
+                    while (sl_first >= 0 && sl_second >= 0);
 
+                    result = (short)(sl_first + sl_second);
+
+                    bool difficult = rand.Next(100) < 50 ? true : false;
+
+                    if (difficult == false)
+                    {
+                        if (sl_second < 0)
+                        {
+                            return $"{sl_first} + ({sl_second}) =";
+                        }
+                        else return $"{sl_first} + {sl_second} =";
+                    }
+                    else
+                    {
+                        if (sl_second < 0)
+                        {
+                            return $"{sl_first} - {sl_second * -1} =";
+                        }
+                        else return $"{sl_first} + {sl_second} =";
                     }
                 }
-                while (sl_first >= 0 && sl_second >= 0);
-
-                result = (short)(sl_first + sl_second);
-
-                bool difficult = rand.Next(100) < 50 ? true : false;
-
-                if (difficult == false)
-                {
-                    if (sl_second < 0)
-                    {
-                        return $"{sl_first} + ({sl_second}) =";
-                    }
-                    else return $"{sl_first} + {sl_second} =";
-                }
-                else
-                {
-                    if (sl_second < 0)
-                    {
-                        return $"{sl_first} - {sl_second * -1} =";
-                    }
-                    else return $"{sl_first} + {sl_second} =";
-                }
-            }
             else
-            {
-                short sl_first, sl_second, sl_third;
-                do
                 {
-                    sl_first = (short)rand.Next(-30, 30);
-                    sl_second = (short)rand.Next(-30, 30);
-                    sl_third = (short)rand.Next(-30, 30);
-                }
-                while (sl_first >= 0 && sl_second >= 0 && sl_third >= 0);
+                    short sl_first, sl_second, sl_third;
+                    do
+                    {
+                        sl_first = (short)rand.Next(-30, 30);
+                        sl_second = (short)rand.Next(-30, 30);
+                        sl_third = (short)rand.Next(-30, 30);
+                    }
+                    while (sl_first >= 0 && sl_second >= 0 && sl_third >= 0);
 
-                result = (short)(sl_first + sl_second + sl_third);
+                    result = (short)(sl_first + sl_second + sl_third);
 
-                if (sl_second < 0 && sl_third < 0)
-                    return $"{sl_first} + ({sl_second}) + ({sl_third}) =";
-                else if (sl_second > 0 && sl_third < 0)
-                    return $"{sl_first} + {sl_second} + ({sl_third}) =";
-                else if (sl_second < 0 && sl_third > 0)
-                    return $"{sl_first} + ({sl_second}) + {sl_third} =";
-                else if (sl_second > 0 && sl_third > 0)
-                    return $"{sl_first} + {sl_second} + {sl_third} =";
-                else return "Попробуйте снова!";
-            }
+                    if (sl_second < 0 && sl_third < 0)
+                        return $"{sl_first} + ({sl_second}) + ({sl_third}) =";
+                    else if (sl_second > 0 && sl_third < 0)
+                        return $"{sl_first} + {sl_second} + ({sl_third}) =";
+                    else if (sl_second < 0 && sl_third > 0)
+                        return $"{sl_first} + ({sl_second}) + {sl_third} =";
+                    else if (sl_second > 0 && sl_third > 0)
+                        return $"{sl_first} + {sl_second} + {sl_third} =";
+                    else return "Попробуйте снова!";
+                }           
         }
 
         private void ClickBackButton(object sender, RoutedEventArgs e)
@@ -118,42 +119,82 @@ namespace Main_Project
 
         private void ClickCheckButton(object sender, RoutedEventArgs e)
         {
-
             try
             {
-                if (Convert.ToInt32(TextBoxExResult.Text) == result)
+                if (taskNumber < 10)
                 {
-                    Message.Content = "Верно!";
-                    Message.Foreground = Brushes.Green;
-                    Message.Visibility = Visibility.Visible;
+                    if (Convert.ToInt32(TextBoxResult.Text) == result)
+                    {
+                        messageLabel.Content = "Верно!";
+                        messageLabel.Foreground = Brushes.Green;
+                        messageLabel.Visibility = Visibility.Visible;
+                        completedTaskNumber++;
+                        updateIsEnabledOfTextBoxResultAndButtonCheck(false);
+                    }
+                    else
+                    {
+                        messageLabel.Content = "Неверно!";
+                        messageLabel.Foreground = Brushes.Red;
+                        messageLabel.Visibility = Visibility.Visible;
+                        updateIsEnabledOfTextBoxResultAndButtonCheck(false);
+                    }
                 }
                 else
                 {
-                    Message.Content = "Неверно!";
-                    Message.Foreground = Brushes.Red;
-                    Message.Visibility = Visibility.Visible;
+                        MessageBox.Show("Вы уже прошли тест!");
                 }
             }
             catch
             {
-                Message.Content = "Возникла ошибка при вводе данных!";
-                Message.Foreground = Brushes.Red;
-                Message.Visibility = Visibility.Visible;
+                messageLabel.Content = "Возникла ошибка при вводе данных!";
+                messageLabel.Foreground = Brushes.Red;
+                messageLabel.Visibility = Visibility.Visible;
             }
 
-            if (TextBoxExResult.Text == "")
-            {
-                Message.Content = "Данные не введены";
-                Message.Foreground = Brushes.Red;
-                Message.Visibility = Visibility.Visible;
-            }
-
+            if (TextBoxResult.Text == "")
+                {
+                    messageLabel.Content = "Данные не введены";
+                    messageLabel.Foreground = Brushes.Red;
+                    messageLabel.Visibility = Visibility.Visible;
+                }
+            
         }
 
         private void ClickNewExerciseButton(object sender, RoutedEventArgs e)
         {
-            TextBoxExResult.Text = ""; Message.Visibility = Visibility.Collapsed;
-            TextBoxExercise.Text = CreateExercise();
+            updateIsEnabledOfTextBoxResultAndButtonCheck(true);
+            if (taskNumber < 10)
+            {
+                TextBoxResult.Text = ""; messageLabel.Visibility = Visibility.Collapsed;
+                updateTaskNumberTextBlock();
+                TextBoxExercise.Text = CreateExercise();
+            }
+            else
+            {
+                MessageBox.Show($"Вы завершили тест, выполнив заданий: {completedTaskNumber}");
+            }
+        }
+
+        private void updateTaskNumberTextBlock()
+        {
+            if (taskNumber < 10)
+            {
+                taskNumber++;
+                taskNumberTextBlock.Text = $"Задание {taskNumber}/10";
+            }
+        }
+        private void updateIsEnabledOfTextBoxResultAndButtonCheck(bool isEnabled)
+        {
+            if(isEnabled)
+            {
+                ButtonCheck.IsEnabled = true;
+                TextBoxResult.IsEnabled = true;
+            }
+            else
+            {
+                ButtonCheck.IsEnabled = false;
+                TextBoxResult.IsEnabled = false;
+            }
         }
     }
 }
