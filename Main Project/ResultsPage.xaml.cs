44a04c36d49d5e7cf2d2ca2e219094173eb35a41
Иван Сабельников <ivansabelnikov3@gmail.com>
2023-02-08 19:19:35 +0300

    Added showing results

--- Main Project/ResultsPage.xaml.cs
+++ Main Project/ResultsPage.xaml.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -21,16 +23,45 @@ namespace Main_Project
     public partial class ResultsPage : Page
     {
         string login;
+        DBLearningMath dBLearningMath = new DBLearningMath();
+
         public ResultsPage(string login)
         {
             InitializeComponent();
             this.login = login;
-
+            ResultsUpdate();
         }
 
         private void ClickBackButton(object sender, RoutedEventArgs e)
         {
             NavigationService.Navigate(new MenuPage(login));
+        }
+
+        public void ResultsUpdate()
+        {
+            string login_check_query = $"SELECT COUNT(login) FROM users WHERE login = '{login}'";
+            string stats_query = $"SELECT res_id AS '№', theme AS 'Тема', score AS 'Результат' FROM results WHERE stud_id = (SELECT user_id FROM users WHERE login = '{login}')";
+
+            dBLearningMath.OpenConnection();
+
+            SqlCommand login_check_command = new SqlCommand(login_check_query, dBLearningMath.GetConnection());
+            SqlCommand stats_command = new SqlCommand(stats_query, dBLearningMath.GetConnection());
+
+            if (Convert.ToInt32(login_check_command.ExecuteScalar()) == 1)
+            {
+                stats_command.ExecuteNonQuery();
+                SqlDataAdapter statsDataAdapter = new SqlDataAdapter(stats_command);
+                DataTable dataTable = new DataTable("Статистика");
+                statsDataAdapter.Fill(dataTable);
+                resultsDataGrid.ItemsSource = dataTable.DefaultView;
+            }
+
+            else
+            {
+                MessageBox.Show("У вас пока нет результатов!");                
+            }
+
+           dBLearningMath.CloseConnection();
         }
     }
 }
